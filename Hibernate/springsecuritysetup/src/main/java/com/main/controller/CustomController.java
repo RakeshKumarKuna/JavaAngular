bb38525927a6d3adc0dab522a30a41c9a407e8b5
Rakesh Kumar Kuna <kunarakeshkumar@gmail.com>
2025-01-04 20:03:12 +0530

    security implemented

---
+++ Hibernate/springsecuritysetup/src/main/java/com/main/controller/CustomController.java
@@ -0,0 +1,40 @@
+package com.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.main.entity.User;
+//https://docs.spring.io/spring-security/site/docs/5.4.3/guides/helloworld-xml.html
+@Controller
+public class CustomController {
+  @RequestMapping(value = {"/","/home"},method = RequestMethod.GET)
+	public String goHome(Model model) {
+	  model.addAttribute("user", new User());
+	  System.out.println("request hit");
+		return "index";
+	}
+	  @PostMapping("/formData") 
+	  public String formSubmit(@ModelAttribute("user") User user,RedirectAttributes attr )
+	  {
+		  System.out.println(user);
+		  String msg="Date Submited with email "+user.getEmail();
+		  attr.addFlashAttribute("msg", msg);
+		  return "redirect:/home"; 
+		  }
+	  @GetMapping("/getdata{id}")
+     public String getData(@RequestParam("id") int id) {
+		  System.out.println("data from get method "+id);
+    	 return "index";
+     }
+	  @GetMapping("/mail")
+	  public String mail() {
+		  return "mail";
+	  }
+}
---
+++ Hibernate/springsecuritysetup/src/main/webapp/WEB-INF/pages/index.jsp
@@ -0,0 +1,26 @@
+<html>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<body>
+<div>
+<h1 style="color:red;text-align:center">Welcome to Spring Web</h1>
+</div>
+<div class="d-flex justify-content-center align-items-center mt-4">
+<form action="formData" method="post" >
+  <dl>
+  <dt>Email</dt>
+  <dd><input name="email" type="text" placeholder="enter your email" class="form-control"></dd>
+  <dt>Password</dt>
+  <dd><input name="password" type="password" placeholder="enter your password" class="form-control"></dd>
+  </dl>
+  <div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="reset"  class="btn btn-danger">Reset</button>
+  </div>
+</form>
+</div>
+<div>
+<span>${msg}</span>
+</div>
+</body>
+</html>
